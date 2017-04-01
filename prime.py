7ddf674388fce9452ce4a1b9c311f5faeae48973
McMunchly <sammyd@samueldassler.com>
2017-03-31 23:53:32 -0700

    check if a number is prime or not

--- prime.py
+++ prime.py
@@ -3,6 +3,7 @@
 def GetPrime(number):
     numRange = range(2, number)
 
+    # check for all divisors that aren't 1 and the number itself
     for element in numRange:
         if number % element == 0:
             return False
