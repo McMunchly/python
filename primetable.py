2018b99f7ca92ee012d4079300b7944f04387f32
McMunchly <sammyd@samueldassler.com>
2017-03-31 23:54:16 -0700

    prints a table of prime numbers

--- primetable.py
+++ primetable.py
@@ -7,8 +7,10 @@ number = int(input("Enter amount of prime numbers: "))
 counter = 1
 primes = 0
 
+# loop until raching the user-inputted number
 while primes < number :
-    
+
+    # check if the number is prime
     if GetPrime(counter) :
         print(counter)
         primes = primes + 1
