56f5de14afad365430569d73f99d642d77c56fe2
McMunchly <sammyd@samueldassler.com>
2017-03-31 23:45:57 -0700

    pig latin comments
    added a bunch of comments to piglatin.py

--- piglatin.py
+++ piglatin.py
@@ -5,21 +5,24 @@ way = "way"
 
 sentence = input("Enter a sentence: ")
 
-newsentence = ""
+newphrase = ""
 
 words = sentence.split(" ")
 
 reverser = 0
 
 def PigLatin(word):
-    # look for consonant clusters
+    # check for upper capitalized words
     upper = False
     if word[0].isupper():
         upper = True
     word = word.lower()
-    
+
+    # vowel sounds stay the same, just as 'way' to the end
     if(word[0] == "a" or word[0] == "e" or word[0] == "i" or word[0] == "o" or word[0] == "u"):
         word = word + "w"
+
+    # consonants up to the first vowel are move to back and 'ay' is added
     else:
         letter = 0
 
@@ -27,6 +30,7 @@ def PigLatin(word):
             letter = letter + 1
         word = word[letter:] + word[0:letter]
 
+    # recapitalize if needed
     if upper == True:
         word = word[0].upper() + word[1:]
 
@@ -35,10 +39,13 @@ def PigLatin(word):
 while reverser < len(words) :
     newword = PigLatin(words[reverser]) + pyg
 
+    # move periods to the back
     if("." in newword):
         newword = newword.replace(".", "")
         newword = newword + "."
-    newsentence = newsentence + newword + " "
+
+    # add word to the phrase
+    newphrase = newphrase + newword + " "
     reverser = reverser + 1
 
-print(newsentence)
+print(newphrase)
