11f33a30e1aa5f3f3a0b6984d33303fb5c40ee2e
McMunchly <sammyd@samueldassler.com>
2017-03-31 23:51:29 -0700

    prints the fibonacci sequence
    added comments

--- fibonacci.py
+++ fibonacci.py
@@ -4,12 +4,16 @@ limit = int(input("Enter number of fibonacci digits to display: "))
 
 numbers = []
 
+# the first two numbers of the sequence are 1
 num1 = 1;
 num2 = 1;
 
 while(limit > 0):
+    # add the first wto digits first
     if(len(numbers) < 2):
         numbers.append(num1)
+
+    # each number is teh sum of the previous two numbers
     else:
         num1 = numbers[len(numbers) - 1]
         num2 = numbers[len(numbers) - 2]
@@ -17,5 +21,6 @@ while(limit > 0):
 
     limit -= 1
 
+# print each digit on it's own line for clarity
 for element in numbers:
     print(element)
