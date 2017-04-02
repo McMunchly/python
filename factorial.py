86e8ed98cacfcf91c0c3792fc9df37c2bd250092
McMunchly <sammyd@samueldassler.com>
2017-04-02 12:12:19 -0700

    prints the factorial of a given numer

---
+++ factorial.py
@@ -0,0 +1,11 @@
+number = int(input("Enter which factorial you want: "))
+
+def factorial(x):
+    total = 1
+    
+    for number in range(1, x + 1):
+        total = total * number
+        
+    return total
+
+print(factorial(number))
