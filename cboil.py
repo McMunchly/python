d4be6d4b20fb4832fa900a59d51f2b4deaaf389f
McMunchly <sammyd@samueldassler.com>
2017-04-11 16:37:03 -0700

    fixed a really bad error where if the filename is taken, it overwrites anyway

--- cboil.py
+++ cboil.py
@@ -4,6 +4,8 @@
 import sys
 import glob
 
+write = True
+
 if len(sys.argv) == 1:
     print("You need to supply a filename")
 
@@ -11,8 +13,10 @@ else:
     for name in glob.glob(sys.argv[1] + ".c"):
         if len(sys.argv) < 3:
             print("filename already in use. File not created")
+            write = False
         elif sys.argv[2] == "o":
             print(sys.argv[1], "was overwritten")
 
-    with open(sys.argv[1] + ".c", "w") as file:
-        file.write("/*  */\n#include <stdio.h>\n\nint main(void)\n{\n    return 0;\n}")
+    if write == True:
+        with open(sys.argv[1] + ".c", "w") as file:
+            file.write("/*  */\n#include <stdio.h>\n\nint main(void)\n{\n    return 0;\n}")
