d52158f028cd85be48e285713bf065ba729f1f1c
McMunchly <sammyd@samueldassler.com>
2017-04-14 12:33:40 -0700

    caesar cipher is now a function and there is a decrypt function too

--- caesar.py
+++ caesar.py
@@ -1,25 +1,55 @@
-# encrypts a phrase with a Ceasar cipher
+# encrypts or decrypts a phrase with a Ceasar cipher
 
-phrase = input("Enter a phrase: ")
-number = int(input("Enter number of letters to shift: "))
-
-coded = ""
 lowerA = 97
 upperA = 65
-upper = 0;
 
-for i in phrase:
-    if str.isalpha(i):
-        if str.isupper(i):
-            upper = upperA
-        else:
-            upper = lowerA
+def CaesarEncrypt(toencrypt, key):
+
+    coded = ""
+    upper = 0;
+    
+    for i in toencrypt:
+        if str.isalpha(i):
+            if str.isupper(i):
+                upper = upperA
+            else:
+                upper = lowerA
+                
+            i = upper + ((ord(i) - upper) + key) % 26
             
-        i = upper + ((ord(i) - upper) + number) % 26
-        
-        coded += chr(i)
-    else:
-        coded += i
+            coded += chr(i)
+        else:
+            coded += i
 
-print(coded);
+    return coded
+
+def CaesarDecrypt(todecrypt, key):
+    
+    coded = ""
+    upper = 0;
+    
+    for i in todecrypt:
+        if str.isalpha(i):
+            if str.isupper(i):
+                upper = upperA
+            else:
+                upper = lowerA
+                
+            i = upper + ((ord(i) - upper) - key) % 26
+            
+            coded += chr(i)
+        else:
+            coded += i
+
+    return coded
+
+if __name__ == "__main__":
+    phrase = input("Enter a phrase: ")
+    number = int(input("Enter number of letters to shift: "))
+
+    print("\nEncrypted:")
+    phrase = CaesarEncrypt(phrase, number)
+    print(phrase)
+    print("\nDecrypted:")
+    print(CaesarDecrypt(phrase, number))
     
