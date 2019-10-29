149c79fb99679f04eaa1649ffb485d8d1dc9ade3
Samuel Dassler <sammyd@samueldassler.com>
2019-10-29 03:06:43 -0700

    Update dnd.py
    now choose class and calculates ability modifers and HP

--- dnd.py
+++ dnd.py
@@ -1,33 +1,56 @@
 import random
+import math
 import dice
 
 random.seed
 
-amount = input("Enter character's name: ")
+abilities = ["Strength", "Dexterity", "Constitution", "Intelligence", "Wisdom", "Charisma"]
+classes = [["Barbarian", dice.d12],["Bard", dice.d8],["Cleric", dice.d8],["Druid", dice.d8],
+        ["Fighter", dice.d10],["Monk", dice.d8],["Paladin", dice.d10],["Ranger", dice.d10],
+        ["Rogue", dice.d8],["Sorcerer", dice.d6],["Warlock", dice.d8],["Wizard", dice.d6]]
+        
+def GetAbilityModifier(score):
+        return int(math.floor((score - 10) / 2))
 
-labels = ["Strength", "Dexterity", "Constitution", "Intelligence", "Wisdom", "Charisma"]
-stats = []
-total = 0
-j = 0
-x = 0;
+def RollCharacter():
+        amount = input("Enter character's name: ")
 
-d6 = 6
+        # loop until the player enters a valid class number
+        characterClass = -1
+        while characterClass < 0 or characterClass > 11:
+                for i in range(len(classes)):
+                        print(str(i) + " - " + classes[i][0] + "\thit die: " + str(classes[i][1]))
+                              
+                characterClass = int(input(": "))
 
-for x in range(len(labels)):
-	print(labels[x] + " rolls: ")
-	y = 0
-	i = 0
+        stats = []
+        total = 0
+        j = 0
+        x = 0;
 
-	rolls = dice.RollDice(4, dice.d6)
+        d6 = 6
 
-	rolls.sort()
-	rolls = rolls[len(rolls) - 3:]
-	print(rolls)
+        for x in range(len(abilities)):
+                print(abilities[x] + " rolls: ")
+                y = 0
+                i = 0
 
-	stats.append(dice.SumDice(rolls))
+                rolls = dice.RollDice(4, dice.d6)
 
+                rolls.sort()
+                rolls = rolls[len(rolls) - 3:]
+                print(rolls)
 
-print("your character's name is:", amount)
+                stats.append(dice.SumDice(rolls))
 
-for j in range(len(labels)):
-	print(labels[j] + ":", stats[j])
+        print()
+        print("your character's name is:", amount)
+        print("your class is: ", classes[characterClass][0])
+
+        print("HP: ", str(classes[characterClass][1] + GetAbilityModifier(stats[2])))
+        
+        for j in range(len(abilities)):
+                print(abilities[j] + ":", stats[j])
+                print("Your modifier is: " + str(GetAbilityModifier(stats[j])))
+
+RollCharacter()
