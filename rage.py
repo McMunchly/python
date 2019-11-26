a06c3b3ce685b3dade1d2e1521a7be3d9edf8778
Samuel Dassler <sammyd@samueldassler.com>
2019-11-26 00:19:17 -0800

    rage.py
    a dumb script that simulates a softlock escape

---
+++ rage.py
@@ -0,0 +1,24 @@
+import random
+random.seed
+print("LORELEI sent out DEWGONG!")
+print("Go! PRIMATE!")
+print("PRIMATE used RAGE!")
+count = int(input("How many tries to escape?: "))
+
+for x in range(0, count):
+    pp = 20
+    while pp > 0:
+        rando = random.randint(0, 256)
+            
+        if rando == 256:
+            print("PRIMATE's attack missed!")
+            pp -= 1
+        else:
+            break
+    
+    if(pp == 0):
+        print("Softlock escaped!!!");
+    else:
+        print("Attempt " + str(x + 1) + ": failed at pp = " + str(pp))
+
+print("Enemy DEWGONG used Rest!")
